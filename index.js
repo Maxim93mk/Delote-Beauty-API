515a87304b513d8cb9ad50ea04f6347d149848ea
Maxim93mk <kinjashov.m@rambler.ru>
2024-05-01 00:13:44 +0300

    API - готов

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express(); 
+const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
@@ -7,11 +7,80 @@ const cors = require('cors');
 
 const PORT = 8000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/price/get-list', (req, res) => {
+    res.status(200)
+    res.send(
+        [
+            {
+                nameService: 'Простая укладка',
+                additionalService: '+ окрашивание',
+                priceService: '500 ₽',
+            },
 
-app.listen(PORT, ()=>{
+            {
+                nameService: 'Event-укладка',
+                additionalService: '+ завивка',
+                priceService: '700 ₽',
+            },
+            {
+                nameService: 'Стрижка',
+                additionalService: '+ выпрямление',
+                priceService: '1500 ₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '4000₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '5000₽',
+            },
+
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '6000₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '7000₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '8000₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '9000₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '10000₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '11000₽',
+            },
+            {
+                nameService: 'Женская укладка',
+                additionalService: '+ обработка кончинков волос',
+                priceService: '12000₽',
+            }
+        ]
+    )
+});
+
+app.listen(PORT, () => {
     console.log("Сервер запущен...")
 });
